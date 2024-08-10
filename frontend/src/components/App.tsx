2b29b07b83ccc5471b7de48ab0aebf32469f10f1
Michał Bielicki <mbielicki2912@gmail.com>
2024-08-10 12:28:48 +0200

    simple frontend

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,7 +1,17 @@
 import * as React from "react";
+import MicIcon from '@mui/icons-material/Mic';
+
 
 export default function App() {
     return (
-        <h1 className="font-bold text-3xl w-full text-center text-blue-800">Hello React!</h1>
+        <main className="w-screen h-screen bg-slate-800
+        flex flex-col items-center gap-5 pt-4">
+            <h1 className="font-bold text-3xl w-full 
+            text-center text-white">Sound analyzer</h1>
+            <MicIcon fontSize="large" className="text-white" />
+            <div className="w-3/4 h-40 bg-slate-400"></div>
+            <div className="w-3/4 h-40 bg-slate-400"></div>
+            <div className="w-3/4 h-40 bg-slate-400"></div>
+        </main>
     )
 }
