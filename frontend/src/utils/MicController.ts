4a2fc2c2d95a198db75a0f2ecdc3c553cc3bc27f
Michał Bielicki <mbielicki2912@gmail.com>
2024-08-10 17:01:43 +0200

    move MicController to utils, add processAudio() to MicController

---
+++ frontend/src/utils/MicController.ts
@@ -0,0 +1,51 @@
+const TIMESLICE = 1000;
+
+class AudioRecorderError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "AudioRecorderError";
+    }
+}
+
+export type ProcessAudio = (e: Blob) => void
+ 
+export class MicController {
+    audioBlobs: Blob[]
+    recorder: MediaRecorder
+
+    constructor() {
+        this.audioBlobs = [];
+        this.recorder = null;
+    }
+
+    async start(processAudio: ProcessAudio) {
+        if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia))
+            return Promise.reject(new AudioRecorderError('mediaDevices API or getUserMedia method is not supported in this browser.'));
+
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+        this.recorder = new MediaRecorder(stream);
+        this.recorder.ondataavailable = e => {
+            processAudio(e.data)
+            this.audioBlobs.push(e.data);
+        };
+
+        this.recorder.onstop = e => {
+            const fullAudio = new Blob(this.audioBlobs);
+            this.audioBlobs = [];
+            const audioURL = window.URL.createObjectURL(fullAudio);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = audioURL;
+            a.download = 'audio-analyzed.wav';
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(() => {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(audioURL);
+            }, 1000);
+        };
+
+        this.recorder.start(TIMESLICE)
+    }
+}
