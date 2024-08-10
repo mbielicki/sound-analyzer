c1478d512c1fb0d4395fdb4433b2a27f75721f3c
Michał Bielicki <mbielicki2912@gmail.com>
2024-08-10 17:55:15 +0200

    AudioRecorder turn on/off

--- frontend/src/components/AudioRecorder.tsx
+++ frontend/src/components/AudioRecorder.tsx
@@ -1,19 +1,40 @@
 import * as React from "react";
 import MicIcon from '@mui/icons-material/Mic';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { MicController, ProcessAudio } from "../utils/MicController";
 
 const processAudio: ProcessAudio = (data) => {
-    console.log(`Audio data length: ${data.size}`)
+    // console.log(`Audio data length: ${data.size}`)
 }
+const micController = new MicController();
 
 export default function AudioRecorder() {
+    const [micIsOn, setMicIsOn] = useState(false)
+
     useEffect(() => {
-        const micController = new MicController();
-        micController.start(processAudio).catch(console.error);
-    });
+        micController.setup(processAudio)
+            .then(() => setMicIsOn(true))
+            .catch(console.error)
+    }, [])
+
+    const onClickMicIcon: React.MouseEventHandler<SVGSVGElement>
+        = e => {
+            if (micIsOn) {
+                setMicIsOn(false);
+                micController.stop()
+            } else {
+                setMicIsOn(true);
+                micController.start()
+            }
+        }
+
     return (
-        <MicIcon fontSize="large" className="text-white" />
+        <MicIcon fontSize="large" onClick={onClickMicIcon}
+            className={
+                (micIsOn ? "text-red-700" : "text-white")
+                + " hover:opacity-75 active:text-slate-900"
+            }
+        />
     )
 
 }
--- frontend/src/utils/MicController.ts
+++ frontend/src/utils/MicController.ts
@@ -1,9 +1,9 @@
-const TIMESLICE = 1000;
+const TIMESLICE = 1000
 
 class AudioRecorderError extends Error {
     constructor(message: string) {
-        super(message);
-        this.name = "AudioRecorderError";
+        super(message)
+        this.name = "AudioRecorderError"
     }
 }
 
@@ -12,40 +12,46 @@ export type ProcessAudio = (e: Blob) => void
 export class MicController {
     audioBlobs: Blob[]
     recorder: MediaRecorder
+    start: () => void
+    stop: () => void
 
     constructor() {
-        this.audioBlobs = [];
-        this.recorder = null;
+        this.audioBlobs = []
+        this.recorder = null
     }
 
-    async start(processAudio: ProcessAudio) {
+    async setup(processAudio: ProcessAudio) {
         if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia))
-            return Promise.reject(new AudioRecorderError('mediaDevices API or getUserMedia method is not supported in this browser.'));
+            return Promise.reject(new AudioRecorderError('mediaDevices API or getUserMedia method is not supported in this browser.'))
 
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
 
-        this.recorder = new MediaRecorder(stream);
+        this.recorder = new MediaRecorder(stream)
         this.recorder.ondataavailable = e => {
             processAudio(e.data)
-            this.audioBlobs.push(e.data);
-        };
+            this.audioBlobs.push(e.data)
+        }
 
         this.recorder.onstop = e => {
-            const fullAudio = new Blob(this.audioBlobs);
-            this.audioBlobs = [];
-            const audioURL = window.URL.createObjectURL(fullAudio);
-            const a = document.createElement('a');
-            a.style.display = 'none';
-            a.href = audioURL;
-            a.download = 'audio-analyzed.wav';
-            document.body.appendChild(a);
-            a.click();
-            setTimeout(() => {
-                document.body.removeChild(a);
-                window.URL.revokeObjectURL(audioURL);
-            }, 1000);
-        };
+            // const fullAudio = new Blob(this.audioBlobs)
+            // this.audioBlobs = [];
+            // const audioURL = window.URL.createObjectURL(fullAudio)
+            // const a = document.createElement('a')
+            // a.style.display = 'none'
+            // a.href = audioURL
+            // a.download = 'audio-analyzed.wav'
+            // document.body.appendChild(a)
+            // a.click()
+            // setTimeout(() => {
+            //     document.body.removeChild(a)
+            //     window.URL.revokeObjectURL(audioURL)
+            // }, 1000)
+        }
+
+        this.start = () => this.recorder.start(TIMESLICE)
+        this.stop = this.recorder.stop.bind(this.recorder)
 
         this.recorder.start(TIMESLICE)
     }
+    
 }
