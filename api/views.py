aafc1fd8ced2c16dbe4d9f919c179275fb14af72
Michał Bielicki <mbielicki2912@gmail.com>
2024-09-02 17:47:33 +0200

    fix sound analysis, wrong dtypes

--- api/views.py
+++ api/views.py
@@ -1,20 +1,18 @@
 from django.http import HttpRequest, JsonResponse
-
-from api.wav_analyzer.analyze import extract_frequencies, plot_frequencies, extract_notes
-from api.wav_analyzer.utils import normalize_chunk
+from api.wav_analyzer.analyze import extract_frequencies, plot_frequencies, extract_notes, remove_wav_headers
 from hashlib import sha1
 
 
 def analyze(request: HttpRequest):
     chunk = request.body
-    normalize_chunk(chunk)
-    yf = extract_frequencies(chunk)
+    remove_wav_headers(chunk)
+    x, y = extract_frequencies(chunk)
 
     hash = sha1(chunk).hexdigest()[:8]
     plot_file = f'api\\static\\api\\plots\\{hash}.png'
-    plot_frequencies(yf, plot_file)
+    plot_frequencies(x, y, plot_file)
 
     return JsonResponse({
-        'notes': extract_notes(yf),
+        'notes': extract_notes(x, y),
         'plot': plot_file[len("api\\"):]
     })
--- api/wav_analyzer/analyze.py
+++ api/wav_analyzer/analyze.py
@@ -2,41 +2,81 @@ import numpy as np
 import numpy.typing as npt
 import matplotlib.pyplot as plt
 import matplotlib
-from scipy.fft import fft
-from .utils import f_to_key, get_maxima
+from scipy.fft import rfft, rfftfreq
 
 import os
 import time
 import threading
 
+type npFloats = npt.NDArray[np.floating]
+type npTuple = tuple[npFloats, npFloats]
+
 matplotlib.use('Agg')
 
-CHUNK_SIZE = 1024 * 8
-CHANNELS = 1
-RATE = 48_000 
+SAMPLE_RATE = 48_000 
 
-xf = np.linspace(0, RATE, CHUNK_SIZE) # frequencies spectrum
+def extract_frequencies(chunk: bytes) -> npTuple:
+        data_np = np.frombuffer(chunk, dtype=np.int16)
 
-def extract_frequencies(chunk: bytes) -> npt.NDArray[np.float64]:
-        data_np = np.frombuffer(chunk, dtype='h')
+        samples_no = len(data_np)
+        samples_ps = SAMPLE_RATE
 
-        yf = fft(data_np)
-        normalized_yf = np.abs(yf[0:CHUNK_SIZE]) / (512 * CHUNK_SIZE)
-        return normalized_yf
+        xf = rfftfreq(samples_no, 1 / samples_ps)
+        yf = np.abs(rfft(data_np))
+        
+        max_freq = 8_000 # higher than B8
+        i_max: int = np.where(xf < max_freq)[0][-1]
 
-def extract_notes(yf: npt.NDArray[np.float64]) -> list[int]:        
+        xf = xf[:i_max]
+        yf = yf[:i_max]
+
+        return xf, yf
+
+def extract_notes(xf: npFloats, yf: npFloats) -> list[int]:        
         piano_keys: list[int] = list()
         for f in get_maxima(xf, yf):
             piano_keys.append(f_to_key(f))
 
         return piano_keys
 
-def plot_frequencies(yf: npt.NDArray[np.float64], plot_file: str):
+
+
+def f_to_key(f: float) -> int:
+    if f > 4100:
+        return -1
+    if f < 28:
+        return -2
+    return int(12 * np.log2(f / 440) + 48)
+
+
+
+def get_maxima(x: npFloats, y: npFloats):
+    maxima = []
+    group_size = 5
+    sensible_unit = max(max(y)/20, 0.05e7)
+    threshold = 0.1e7
+    for i in range(0, len(y) - group_size):
+        group = y[i: i + group_size]
+        gmax = max(group)
+        if gmax < threshold:
+             continue
+        gmin = min(group)
+        diff = gmax - gmin
+        if diff < sensible_unit:
+            continue
+        j, = np.where(group == gmax)[0]
+        if j == 0 or j == group_size - 1:
+            continue
+        maxima.append(x[i+j])
+
+    return list(dict.fromkeys(maxima))
+
+def plot_frequencies(xf: npFloats, yf: npFloats, plot_file: str) -> None:
         fig, ax = plt.subplots()
         ax.set(xlabel='Frequency (Hz)', ylabel='Amplitude')
         ax.set_xscale('log')
-        ax.set_xlim(28, 4000)
-        ax.set_ylim(0, 10)
+        ax.set_xlim(0, 8000)
+        ax.set_ylim(0, 1e7)
         ax.plot(xf, yf)
         fig.savefig(plot_file)
 
@@ -45,3 +85,8 @@ def plot_frequencies(yf: npt.NDArray[np.float64], plot_file: str):
             os.remove(plot_file)
 
         threading.Thread(target=delete_plot_file).start()
+
+        
+def remove_wav_headers(chunk: bytes) -> None:
+    if chunk[:4] == b'RIFF':
+        chunk = chunk[44:]
